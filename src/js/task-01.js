54fd138f1919f3a3686c00bc47a8bb2c5aacf0ff
Oleh Kliapko <goldclub1977@gmail.com>
2022-10-13 21:32:56 +0300

    Task #1 is DONE

---
+++ src/js/task-01.js
@@ -0,0 +1,19 @@
+console.log(
+  "Number of categories: ",
+  document.querySelectorAll(".item").length
+);
+
+const categoryTitles = [];
+document.querySelectorAll(".item h2").forEach(function (title) {
+  categoryTitles.push(title.textContent);
+});
+
+const subCategoryCounts = [];
+document.querySelectorAll(".item ul").forEach(function (subCategory) {
+  subCategoryCounts.push(subCategory.children.length);
+});
+
+for (let i = 0; i < categoryTitles.length; i += 1) {
+  console.log("Category: ", categoryTitles[i]);
+  console.log("Elements: ", subCategoryCounts[i]);
+}
