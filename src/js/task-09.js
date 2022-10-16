c225c765804c0539293f9d9fa67412e1edcdc05f
Oleh Kliapko <goldclub1977@gmail.com>
2022-10-16 15:26:14 +0300

    Task #9 is DONE

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,16 @@
+const refs = {
+  bodyEl: document.querySelector("body"),
+  hexColorText: document.querySelector(".color"),
+  buttonChangeColor: document.querySelector(".change-color"),
+};
+
+refs.buttonChangeColor.addEventListener("click", onClickButton);
+
+function onClickButton(event) {
+  refs.bodyEl.style.backgroundColor = getRandomHexColor();
+  refs.hexColorText.textContent = getRandomHexColor();
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
